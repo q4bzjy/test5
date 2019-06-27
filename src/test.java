7926835ca22a604f382af8228f932d0fd9ed276c
q4bzjy <q4bzjy@163.com>
2019-06-27 18:11:14 +0800

    666

--- src/test.java
+++ src/test.java
@@ -3,5 +3,7 @@ import com.sun.org.apache.xpath.internal.SourceTree;
 public class test {
     public static void main(String[] args) {
         System.out.println("helloword");
+
+        System.out.println(666);
     }
 }
